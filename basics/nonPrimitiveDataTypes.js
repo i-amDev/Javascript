ee14b265fff795d3c7c1a1fb2b406823abb0caf0
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-10-14 17:57:24 +0530

    - Learned about non-primitive data types.

---
+++ basics/nonPrimitiveDataTypes.js
@@ -0,0 +1,16 @@
+// Arrays
+let arr = [1, 2, 3];
+console.log(arr)
+
+// Objects
+let myObject = {
+    name: "John",
+    age: 32,
+}
+console.log(myObject)
+
+// Functions
+let myFunction = function () {
+    console.log("Inside Function")
+}
+myFunction();
