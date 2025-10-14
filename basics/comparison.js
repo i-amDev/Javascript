32d46d89854b3231ad766d838ddd4fa455c4c91d
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-10-14 17:43:34 +0530

    - Learned about comparisons.

---
+++ basics/comparison.js
@@ -0,0 +1,9 @@
+console.log(2 > 2)
+console.log(2 >= 2)
+console.log(2 < 2)
+console.log(2 <= 2)
+console.log(2 == 2)
+console.log(2 != 2)
+console.log("2" ==  2)
+console.log("2" === 2)
+console.log(2 === 2) // strict checking i.e. data type is also getting checked.
