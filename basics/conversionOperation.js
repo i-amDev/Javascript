30564ea6e09eecdb98e6a2e18085aced85b69190
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-10-14 17:33:47 +0530

    - Learned about operations.

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,15 +1,25 @@
-let age = "20abc"
+// let age = "20abc"
+//
+// console.log(typeof age)
+// console.log(typeof(age))
+//
+// let valueInNumber = Number(age)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // NaN(Not a Number)
+//
+// let isLoggedIn = true;
+// console.log(typeof isLoggedIn)
+//
+// let numberIsLoggedIn = Number(isLoggedIn)
+// console.log(typeof(numberIsLoggedIn))
+// console.log(numberIsLoggedIn)
 
-console.log(typeof age)
-console.log(typeof(age))
+// OPERATIONS
 
-let valueInNumber = Number(age)
-console.log(typeof(valueInNumber))
-console.log(valueInNumber) // NaN(Not a Number)
+console.log("Addition " + (2 + 2))
+console.log("Subtraction " + (2 - 2))
+console.log("Multiplication " + (2 * 2))
+console.log("Power " + (2 ** 3))
+console.log("Division " + (2 / 2))
+console.log("Modulo " + (2 % 2))
 
-let isLoggedIn = true;
-console.log(typeof isLoggedIn)
-
-let numberIsLoggedIn = Number(isLoggedIn)
-console.log(typeof(numberIsLoggedIn))
-console.log(numberIsLoggedIn)
