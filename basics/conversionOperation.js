90ea080cab6f8de2c191242c8d99a2dfdd81a763
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-10-14 17:17:10 +0530

    - Learned about data type conversion.

---
+++ basics/conversionOperation.js
@@ -0,0 +1,15 @@
+let age = "20abc"
+
+console.log(typeof age)
+console.log(typeof(age))
+
+let valueInNumber = Number(age)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber) // NaN(Not a Number)
+
+let isLoggedIn = true;
+console.log(typeof isLoggedIn)
+
+let numberIsLoggedIn = Number(isLoggedIn)
+console.log(typeof(numberIsLoggedIn))
+console.log(numberIsLoggedIn)
