139e1764fce4ea65cc3c6dde098d6995a0049cc2
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-10-15 13:35:27 +0530

    - Learned about control flows in JS.

---
+++ basics/controlFlow.js
@@ -0,0 +1,20 @@
+let temperature = 13;
+
+if (temperature > 20) {
+    console.log("Temperature is " + temperature);
+}
+else {
+    console.log("Temperature is low!!");
+}
+
+let month = 2;
+switch (month) {
+    case 1 :
+        console.log("January");
+        break;
+    case 2 :
+        console.log("February");
+        break;
+    default :
+        console.log("Invalid");
+}
