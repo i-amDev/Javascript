1ba0ffff2cb43a052f29f3a31bd3c66a045ce208
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-10-17 16:19:20 +0530

    - Unlimited Color

---
+++ projects/UnlimitedColor/logic.js
@@ -0,0 +1,28 @@
+// generate a random color
+
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
+
+let intervalId;
+function startChangingColor() {
+    intervalId = setInterval(changeBackgroundColor, 1000);
+
+    function changeBackgroundColor() {
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+function stopChangingColor() {
+    clearInterval(intervalId);
+}
