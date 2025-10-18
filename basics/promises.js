c9341d414625b2a80e6263252cc2bc386270fcd2
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-10-18 10:48:28 +0530

    - Promises in JS.

--- basics/promises.js
+++ basics/promises.js
@@ -1,58 +1,58 @@
-// const promise = new Promise((resolve, reject) => {
-//     // Do an async task
-//     // DB calls, cryptography, network
-//
-//     setTimeout(function () {
-//         console.log("Async task is completed!!");
-//         resolve(); // to connect the resolve to the then function.
-//     }, 1000);
-// });
-//
-// promise.then(function () {
-//     console.log("Promise consumed!!");
-// });
-//
-// new Promise(function (resolve, reject) {
-//     setTimeout(function (){
-//         console.log("Async task 2!!");
-//         resolve();
-//     }, 1000)
-// }).then(function(){
-//     console.log("Async 2 consumed!!")
-// });
-//
-// const promiseThree = new Promise(function (resolve, reject) {
-//     setTimeout(function (){
-//         resolve({username : "Tony", email : "tony@gmail.com"});
-//     }, 1000);
-// });
-//
-// promiseThree.then(function (data) {
-//     console.log(data);
-// });
-//
-// const promiseFour = new Promise(function (resolve, reject) {
-//     setTimeout(function (){
-//         let error = true;
-//         if (!error) {
-//             resolve({username : "Steve", email : "steve@gmail.com"})
-//         }
-//         else {
-//             reject("Oops, something went wrong!");
-//         }
-//     })
-// })
-//
-// // promiseFour.then(function () {}).catch(function (error) {});
-//
-// promiseFour.then((data) => {
-//     console.log(data);
-//     return data.username;
-// }).then(username => {
-//     console.log(username);
-// }).catch(error => {
-//     console.log(error);
-// }).finally(() => console.log("The promise is either resolved or rejected!!"));
+const promise = new Promise((resolve, reject) => {
+    // Do an async task
+    // DB calls, cryptography, network
+
+    setTimeout(function () {
+        console.log("Async task is completed!!");
+        resolve(); // to connect the resolve to the then function.
+    }, 1000);
+});
+
+promise.then(function () {
+    console.log("Promise consumed!!");
+});
+
+new Promise(function (resolve, reject) {
+    setTimeout(function (){
+        console.log("Async task 2!!");
+        resolve();
+    }, 1000)
+}).then(function(){
+    console.log("Async 2 consumed!!")
+});
+
+const promiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function (){
+        resolve({username : "Tony", email : "tony@gmail.com"});
+    }, 1000);
+});
+
+promiseThree.then(function (data) {
+    console.log(data);
+});
+
+const promiseFour = new Promise(function (resolve, reject) {
+    setTimeout(function (){
+        let error = true;
+        if (!error) {
+            resolve({username : "Steve", email : "steve@gmail.com"})
+        }
+        else {
+            reject("Oops, something went wrong!");
+        }
+    })
+})
+
+// promiseFour.then(function () {}).catch(function (error) {});
+
+promiseFour.then((data) => {
+    console.log(data);
+    return data.username;
+}).then(username => {
+    console.log(username);
+}).catch(error => {
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected!!"));
 
 const promiseFive = new Promise(function (resolve, reject) {
     setTimeout(function (){
